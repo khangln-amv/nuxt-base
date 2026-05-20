e26ec01dfe4fb377e5ab2e71a4057493409307ab
khangln-amv <nguyenkhang-le@am-bition.vn>
2026-05-20 09:55:48 +0700

    feat: init project

---
+++ nuxt.config.ts
@@ -0,0 +1,45 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  modules: [
+    '@nuxt/eslint',
+    '@nuxt/ui'
+  ],
+
+  devtools: {
+    enabled: true
+  },
+
+  css: ['~/assets/css/main.css'],
+
+  routeRules: {
+    '/': { prerender: true }
+  },
+
+  future: {
+    compatibilityVersion: 5,
+  },
+
+  experimental: {
+    nitroAutoImports: true,
+  },
+
+  compatibilityDate: '2025-01-15',
+
+  eslint: {
+    config: {
+      stylistic: {
+        commaDangle: 'only-multiline',
+        semi: true,
+        braceStyle: '1tbs',
+      }
+    }
+  },
+
+  fonts: {
+    // families: [
+    //   { name: 'Custom Font', provider: 'none' },
+    //   { name: 'My Font Family', provider: 'google' },
+    //   { name: 'Other Font', src: 'https://example.com/font.woff2' },
+    // ]
+  },
+});
